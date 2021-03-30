a872be4998ebc4bede2a790addda15899b513a3b
Williamson911 <william.williamson.91@gmail.com>
2021-03-30 10:58:30 +0200

    connection

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -9,25 +9,18 @@ $(function () {
         submitSuccess: function ($form, event) {
             event.preventDefault(); // prevent default submit behaviour
             // get values from FORM
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var phone = $("input#phone").val();
-            var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
+            var userName = $("input#username").val();
+            var password = $("input#pwd").val();
+            // For Success/Failure Message
             // Check for white space in name for Success/Fail message
-            if (firstName.indexOf(" ") >= 0) {
-                firstName = name.split(" ").slice(0, -1).join(" ");
-            }
             $this = $("#sendMessageButton");
             $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
             $.ajax({
                 url: "/assets/mail/contact_me.php",
                 type: "POST",
                 data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message,
+                    username: username,
+                    password: password,
                 },
                 cache: false,
                 success: function () {
@@ -56,7 +49,7 @@ $(function () {
                     $("#success > .alert-danger").append(
                         $("<strong>").text(
                             "Sorry " +
-                                firstName +
+                                userName +
                                 ", it seems that my mail server is not responding. Please try again later!"
                         )
                     );
@@ -83,6 +76,6 @@ $(function () {
 });
 
 /*When clicking on Full hide fail/success boxes */
-$("#name").focus(function () {
+$("#username").focus(function () {
     $("#success").html("");
 });
