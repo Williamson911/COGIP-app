5a6a0e2dcd66fdaf82715886e3e3a8923b608058
Williamson911 <william.williamson.91@gmail.com>
2021-03-31 14:57:29 +0200

    test

--- docker-environment/src/index.php
+++ docker-environment/src/index.php
@@ -3,9 +3,9 @@
 declare(strict_types=1);
 
 //include all your model files here
-require 'docker-environment/src/Model/CompaniesManager.php';
+require 'Model/CompaniesManager.php';
 //include all your controllers here
-require 'docker-environment/src/Controller/HomepageController.php';
+require 'Controller/HomepageController.php';
 
 //Your index is your Router. You could write a simple IF here based on some $_GET or $_POST vars, to choose your controller
 
