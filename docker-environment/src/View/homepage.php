93fc089acbb3ef5690b4987decc676a3e5a3118c
Williamson911 <william.williamson.91@gmail.com>
2021-03-31 16:51:16 +0200

    companiestyle

--- docker-environment/src/View/homepage.php
+++ docker-environment/src/View/homepage.php
@@ -1,16 +1,129 @@
-<?php require 'includes/header.php'?>
-<!-- this is the view, try to put only simple if's and loops here.
-Anything complex should be calculated in the model -->
-<section>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>COGIP</title>
+    <link rel="icon" type="image/x-icon" href="./assets/img/favicon.ico" />
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="COGIP-app/css/styles.css" rel="stylesheet" />
+</head>
+<body id="page-top">
 
-	<h1>Welcome to the COGIP!</h1>
-    <ol>
-    	<?php 
-    		foreach ($companies->getCompanies() as $key => $company) { ?>
-    		<li><p><?php echo $company['Name']?></p></li>	
-    		
-    	<?php } ?>
-    </ol>
+<!-- Navigation-->
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+    <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="https://i.ibb.co/7Rh7NNg/24b99a4eedf226ba.png" alt="cogip" border="0" /></a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            Menu
+            <i class="fas fa-bars ml-1"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav text-uppercase ml-auto">
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#services">Home</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#portfolio">Invoices</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">Companies</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#contact">Contacts</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="docker-environment/src/View/login.php">Connexion</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- Masthead-->
+<header class="masthead">
+    <div class="container">
+        <div class="masthead-heading text-uppercase">Welcome To The COGIP!</div>
+        <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
+    </div>
+</header>
+<!-- Home-->
+<section class="page-section" id="services">
+    <div class="container">
+        <div class="text-center">
+            <h2 class="section-heading text-uppercase">Bonjour!</h2>
+        </div>
+        <div class="row text-center">
 
+            <div class="col-md-3">
+                <h3> Last Invoice :</h3>
+            </div>
+
+            <div class="col-md-3">
+                <h3> Last Contacts :</h3>
+            </div>
+
+            <div class="col-md-3">
+                <h3> Last Companies :</h3>
+            </div>
+        </div>
+    </div>
 </section>
-<?php require 'includes/footer.php'?>
+<!-- Invoices -->
+<section class="page-section bg-light" id="portfolio">
+    <div class="container">
+        <div class="text-center">
+            <h2 class="section-heading text-uppercase">COGIP : List of Invoices</h2>
+        </div>
+    </div>
+</section>
+<!-- Companies -->
+<section class="page-section" id="about">
+    <div class="container">
+        <div class="text-center">
+            <h2 class="section-heading text-uppercase">COGIP : Companies Directory</h2>
+            <ol>
+                <?php
+                foreach ($companies->getCompanies() as $key => $company) { ?>
+                    <li><p><?php echo $company['Name']?></p></li>
+
+                <?php } ?>
+            </ol>
+        </div>
+    </div>
+</section>
+
+<!-- Contact-->
+<section class="page-section" id="contact">
+    <div class="container">
+        <div class="text-center">
+            <h2 class="section-heading text-uppercase">Contact Directory</h2>
+        </div>
+    </div>
+</section>
+<!-- Footer-->
+<footer class="footer py-4">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-4 text-lg-left">Copyright © COGIP 2020</div>
+            <div class="col-lg-4 my-3 my-lg-0">
+                <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
+                <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
+                <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
+            </div>
+            <div class="col-lg-4 text-lg-right">
+                <a class="mr-3" href="#!">Privacy Policy</a>
+                <a href="#!">Terms of Use</a>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- Bootstrap core JS-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Third party plugin JS-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+<!-- Contact form JS-->
+<script src="assets/mail/jqBootstrapValidation.js"></script>
+<script src="assets/mail/contact_me.js"></script>
+<!-- Core theme JS-->
+<script src="js/scripts.js"></script>
+</body>
+</html>
