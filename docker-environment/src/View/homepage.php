ebb859c70b554e237b2b1b6b950cade5159e093f
Williamson911 <william.williamson.91@gmail.com>
2021-04-01 10:21:43 +0200

    homepage

--- docker-environment/src/View/homepage.php
+++ docker-environment/src/View/homepage.php
@@ -159,7 +159,7 @@
 <footer class="footer py-4">
     <div class="container">
         <div class="row align-items-center">
-            <div class="col-lg-4 text-lg-left">Copyright © COGIP 2020</div>
+            <div class="col-lg-4 text-lg-left">Copyright © COGIP <?php echo date('Y')?></div>
             <div class="col-lg-4 my-3 my-lg-0">
                 <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
                 <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
