ec5a02cd66f38a6f36423f1bfaa88b74d0d7d441
Williamson911 <william.williamson.91@gmail.com>
2021-04-08 09:58:12 +0200

    header admin

--- docker-environment/src/View/includes/headerAdmin.php
+++ docker-environment/src/View/includes/headerAdmin.php
@@ -1,28 +1,50 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
     <title>COGIP</title>
+    <link rel="icon" type="image/x-icon" href="./assets/img/favicon.ico" />
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="./css/styles.css" rel="stylesheet" />
 </head>
 
 <body>
-    <header>
-        <h1>Welcome to the COGIP!</h1>
-        <ul>
-            <li><a href="/index.php">Home</a></li>
-            <li><a href="/index.php?page=companies">Companies</a></li>
-            <li><a href="/index.php?page=invoices">Invoices</a></li>
-            <li><a href="/index.php?page=contacts">Contacts</a></li>
-            <li><a href="/index.php?page=moderation">Admin</a></li>
-            <li>
+<header class="masthead homepage">
+    <div class="container">
+        <div class="masthead-heading text-uppercase">Welcome To The COGIP!</div>
+        <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="https://i.ibb.co/7Rh7NNg/24b99a4eedf226ba.png" alt="cogip" border="0" /></a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars ml-1"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav text-uppercase ml-auto">
+            <li class="nav-item"><a class="nav-link" href="/index.php">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="/index.php?page=companies">Companies</a></li>
+            <li class="nav-item"><a class="nav-link" href="/index.php?page=invoices">Invoices</a></li>
+            <li class="nav-item"><a class="nav-link" href="/index.php?page=contacts">Contacts</a></li>
+            <li class="nav-item"><a class="nav-link" href="/index.php?page=moderation">Admin</a></li>
+                    </ul>
+                        <div class="text-center">
                 <form action="" method="POST">
-                    <button type="submit" name="deconnexion">Log out</button>
+                    <button class="btn btn-danger btn-xl text-uppercase" type="submit" name="deconnexion">Log out</button>
                 </form>
-            </li>
-        </ul>
+                        </div>
+
+                </div>
+            </div>
+        </nav>
     </header>
