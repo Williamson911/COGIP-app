408c90f62717ba5124fb606c139c90aced6916fc
Williamson911 <william.williamson.91@gmail.com>
2021-04-09 09:55:02 +0200

    var env

--- src/Model/Manager.php
+++ src/Model/Manager.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 require 'environment.php';
 
+
 class Manager
-{
+{ $user = getenv('USER');
+$password = getenv('PASSWORD');
 	protected function connectDb()
 	{
 		try {
-			$db = new PDO("mysql:host=remotemysql.com;dbname=S4vXv3ajmJ;port=3306", $_ENV["USER"], $_ENV["PASSWORD"]);
+			$db = new PDO("mysql:host=remotemysql.com;dbname=S4vXv3ajmJ;port=3306", $user, $password);
 			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			return $db;
 		} catch (Exception $e) {
