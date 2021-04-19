2f0165972c5b3a5649c35629589442e9810749a0
CharlotteMoureau <Charlotte.Moureau96@gmail.com>
2021-04-19 12:11:48 +0200

    correct invoices' details

--- src/View/detailInvoice.php
+++ src/View/detailInvoice.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
@@ -16,69 +17,68 @@
     <!-- Core theme CSS (includes Bootstrap)-->
     <link href="/css/styles.css" rel="stylesheet" />
 </head>
+
 <body id="page-top">
 
-<!-- Navigation-->
-<?php include($_SERVER['DOCUMENT_ROOT'] . '/View/includes/nav.php'); ?>
-<!-- Masthead-->
-<header class="masthead invoices">
-    <div class="container">
-        <div class="masthead-heading text-uppercase">Invoices Directory</div>
-        <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#invoices">Tell Me More</a>
-    </div>
-</header>
-
-<!-- Contact -->
-<section class="page-section bg-light" id="invoices">
-    <div class="container">
-        <div class="text-center">
-            <h2>COGIP: List of invoices</h2>
+    <!-- Navigation-->
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/View/includes/nav.php'); ?>
+    <!-- Masthead-->
+    <header class="masthead invoices">
+        <div class="container">
+            <div class="masthead-heading text-uppercase">Invoices Directory</div>
+            <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#invoices">Tell Me More</a>
         </div>
-        <div>
-            <h2>Invoice: <?= $detailInvoice['InvoiceNumber'] ?></h2>
+    </header>
 
-            <h3>Company linked to the invoice</h3>
-            <table>
-                <tr>
-                    <th>Name</th>
-                    <th>VAT</th>
-                    <th>Company type</th>
-                </tr>
-                <tr>
-                    <td><?= $detailInvoice['Name'] ?></td>
-                    <td><?= $detailInvoice['VATNumber'] ?></td>
-                    <td><?= $detailInvoice['Type'] ?></td>
-                </tr>
-            </table>
+    <!-- Contact -->
+    <section class="page-section bg-light" id="invoices">
+        <div class="container">
+            <div class="text-center">
+                <h2>COGIP: List of invoices</h2>
+            </div>
+            <div>
+                <h2>Invoice: <?= $detailInvoice['InvoiceNumber'] ?></h2>
 
-            <h3>Contact person</h3>
-            <table>
-                <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                </tr>
-                <tr>
-                    <td><?= $detailInvoice['FirstName'] . ' ' . $detailInvoice['LastName'] ?></td>
-                    <td><?= $detailInvoice['Email'] ?></td>
-                </tr>
-            </table>
+                <h3>Company linked to the invoice</h3>
+                <table>
+                    <tr>
+                        <th>Name</th>
+                        <th>VAT</th>
+                        <th>Company type</th>
+                    </tr>
+                    <tr>
+                        <td><?= $detailInvoice['Name'] ?></td>
+                        <td><?= $detailInvoice['VATNumber'] ?></td>
+                        <td><?= $detailInvoice['Type'] ?></td>
+                    </tr>
+                </table>
+
+                <h3>Contact person</h3>
+                <table>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                    </tr>
+                    <tr>
+                        <td><?= $detailInvoice['FirstName'] . ' ' . $detailInvoice['LastName'] ?></td>
+                        <td><?= $detailInvoice['Email'] ?></td>
+                    </tr>
+                </table>
+            </div>
         </div>
-    </div>
-</section>
-<!-- Bootstrap core JS-->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
-<!-- Third party plugin JS-->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
-<!-- Contact form JS-->
-<script src="/assets/mail/jqBootstrapValidation.js"></script>
-<script src="/assets/mail/contact_me.js"></script>
-<!-- le lien jq permet de faire un prévalidation du form-->
-<!-- Core theme JS-->
-<script src="/js/scripts.js"></script>     
+    </section>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Third party plugin JS-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+    <!-- Contact form JS-->
+    <script src="/assets/mail/jqBootstrapValidation.js"></script>
+    <script src="/assets/mail/contact_me.js"></script>
+    <!-- le lien jq permet de faire un prévalidation du form-->
+    <!-- Core theme JS-->
+    <script src="/js/scripts.js"></script>
 </body>
+
 </html>
 <?php require 'includes/footer.php' ?>
-
-
-
--- src/View/invoices.php
+++ src/View/invoices.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
@@ -16,58 +17,62 @@
     <!-- Core theme CSS (includes Bootstrap)-->
     <link href="./css/styles.css" rel="stylesheet" />
 </head>
+
 <body id="page-top">
 
-<!-- Navigation-->
-<?php include($_SERVER['DOCUMENT_ROOT'] . '/View/includes/nav.php'); ?>
-<!-- Masthead-->
-<header class="masthead invoices">
-    <div class="container">
-        <div class="masthead-heading text-uppercase">Invoices Directory</div>
-    <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#invoices">Tell Me More</a>
-    </div>
-</header>
-
-<!-- Contact -->
-<section class="page-section bg-light" id="invoices">
-    <div class="container">
-        <div class="text-center">
-            <h2>COGIP: List of invoices</h2>
+    <!-- Navigation-->
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/View/includes/nav.php'); ?>
+    <!-- Masthead-->
+    <header class="masthead invoices">
+        <div class="container">
+            <div class="masthead-heading text-uppercase">Invoices Directory</div>
+            <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#invoices">Tell Me More</a>
         </div>
-        <div>
-            <table>
-                <tr>
-                    <th>Invoice number</th>
-                    <th>Dates</th>
-                    <th>Companies</th>
-                    <th>Type</th>
-                </tr>
-                <tr>
-                    <td class="pl-2"><?php foreach ($invoices->getInvoice() as $key => $invoice) { ?>
-                            <p><a href="/invoices/<?=$invoice['InvoiceNumber']?>"><?= $invoice['InvoiceNumber']?></a></p><?php } ?>
-                    <td class="pl-2"><?php foreach ($invoices->getInvoice() as $key => $invoice) { ?>
-                            <p><?php echo $invoice['InvoiceDate']?></p><?php } ?></td>
-                    <td class="pl-2"><?php foreach ($invoices->getInvoice() as $key => $invoice) { ?>
-                            <p><?php echo $invoice['Name']?></p><?php } ?></td>
-                    <td class="pl-2"><?php foreach ($invoices->getInvoice() as $key => $invoice) { ?>
-                            <p><?php echo $invoice['Type']?></p><?php } ?></td>
-                </tr>
-            </table>
-        </div>
-    </div>
-</section>
+    </header>
 
-<!-- Bootstrap core JS-->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
-<!-- Third party plugin JS-->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
-<!-- Contact form JS-->
-<script src="assets/mail/jqBootstrapValidation.js"></script>
-<script src="assets/mail/contact_me.js"></script>
-<!-- Core theme JS-->
-<script src="js/scripts.js"></script>
+    <!-- Contact -->
+    <section class="page-section bg-light" id="invoices">
+        <div class="container">
+            <div class="text-center">
+                <h2>COGIP: List of invoices</h2>
+            </div>
+            <div>
+                <table>
+                    <tr>
+                        <th>Invoice number</th>
+                        <th>Dates</th>
+                        <th>Companies</th>
+                        <th>Type</th>
+                    </tr>
+                    <tr>
+                        <td class="pl-2"><?php foreach ($invoices->getInvoice() as $key => $invoice) { ?>
+                                <p><a href="/invoices/<?= $invoice['Id'] ?>"><?= $invoice['InvoiceNumber'] ?></a></p><?php } ?>
+                        <td class="pl-2"><?php foreach ($invoices->getInvoice() as $key => $invoice) { ?>
+                                <p><?php echo $invoice['InvoiceDate'] ?></p><?php } ?>
+                        </td>
+                        <td class="pl-2"><?php foreach ($invoices->getInvoice() as $key => $invoice) { ?>
+                                <p><?php echo $invoice['Name'] ?></p><?php } ?>
+                        </td>
+                        <td class="pl-2"><?php foreach ($invoices->getInvoice() as $key => $invoice) { ?>
+                                <p><?php echo $invoice['Type'] ?></p><?php } ?>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </section>
+
+    <!-- Bootstrap core JS-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Third party plugin JS-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+    <!-- Contact form JS-->
+    <script src="assets/mail/jqBootstrapValidation.js"></script>
+    <script src="assets/mail/contact_me.js"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
 </body>
+
 </html>
 <?php require 'includes/footer.php' ?>
-
