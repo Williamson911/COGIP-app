1d30ad9e89da702b556fc53531e1e887e6e7c0e8
Williamson911 <william.williamson.91@gmail.com>
2021-04-19 14:35:58 +0200

    edit style and admin header

--- src/View/editContact.php
+++ src/View/editContact.php
@@ -1,18 +1,27 @@
 <?php require 'includes/headerAdmin.php' ?>
 
 
-<section>Edit a contact</section>
-
+<section class="page-section bg-light">
+    <div class="container">
+        <div class="text-center">
+            <h2 class="section-heading text-uppercase">Edit a contact</h2>
+        </div>
 <form action="" method="POST">
+    <div class="row align-items-stretch mb-5">
+        <div class="col-md-6">
+            <div class="form-group">
     <label>Firstname</label>
     <input id="firstname" name="firstname" type="text" required="required" value="<?= $detailContact['FirstName'] ?>">
-    <br>
+            </div>
+            <div class="form-group">
     <label>Lastname</label>
     <input id="lastname" name="lastname" type="text" required="required" value="<?= $detailContact['LastName'] ?>">
-    <br>
+            </div>
+            <div class="form-group">
     <label>Email</label>
     <input id="email" name="email" type="email" required="required" value="<?= $detailContact['Email'] ?>">
-    <br>
+            </div>
+            <div class="form-group">
     <label>Company</label>
     <select name="company" id="company" required="required">
         <?php foreach ($admin->getCompanies() as $key => $company) : ?>
@@ -21,7 +30,28 @@
                                                     } ?>><?= $company['Name'] ?></option>
         <?php endforeach ?>
     </select>
-    <br>
-    <button type="submit" name="editContact">Submit</button>
-    <button><a href="/index.php?page=admin">Cancel</a></button>
+            </div>
+        </div>
+            <div class="text-center">
+    <button class="btn btn-primary btn-xl text-uppercase" type="submit" name="editContact">Submit</button>
+    <button class="btn btn-danger btn-xl text-uppercase"><a href="/index.php?page=admin">Cancel</a></button>
+            </div>
+
 </form>
+
+    </div>
+</section>
+
+    <!-- Bootstrap core JS-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Third party plugin JS-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+    <!-- Contact form JS-->
+    <!-- <script src="assets/mail/jqBootstrapValidation.js"></script> -->
+    <script src="assets/mail/contact_me.js"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+    </body>
+    </html>
+<?php require 'includes/footer.php' ?>
