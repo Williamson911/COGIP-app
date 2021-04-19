1708d90bfcf29e4f72db2693b055e43e2f3bac65
CharlotteMoureau <Charlotte.Moureau96@gmail.com>
2021-04-19 11:49:21 +0200

    correct path for index.php

--- src/index.php
+++ src/index.php
@@ -14,14 +14,14 @@ if (file_exists(__DIR__ . '/.env')) {
 }
 
 //include all your controllers here
-require './src/Controller/HomepageController.php';
-require './src/Controller/CompanyController.php';
-require './src/Controller/InvoiceController.php';
-require './src/Controller/ContactController.php';
-require './src/Controller/ProvidersController.php';
-require './src/Controller/ClientController.php';
-require './src/Controller/ConnexionController.php';
-require './src/Controller/AdminController.php';
+require './Controller/HomepageController.php';
+require './Controller/CompanyController.php';
+require './Controller/InvoiceController.php';
+require './Controller/ContactController.php';
+require './Controller/ProvidersController.php';
+require './Controller/ClientController.php';
+require './Controller/ConnexionController.php';
+require './Controller/AdminController.php';
 
 //Your index is your Router. You could write a simple IF here based on some $_GET or $_POST vars, to choose your controller
 
